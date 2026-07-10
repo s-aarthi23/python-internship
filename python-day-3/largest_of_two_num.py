bfdbe1b801c5b4eb52706942951bf0161d1bb2e3
s-aarthi23 <s.aarthi2308@gmail.com>
2026-07-10 21:59:15 +0530

    Add files via upload

---
+++ python-day-3/largest_of_two_num.py
@@ -0,0 +1,6 @@
+num1=int(input("Enter the number:"))
+num2=int(input("Enter the second number:"))
+if (num1>num2):
+    print("The largest number is:",num1)
+else:
+    print("The largest number is:",num2)
