9bd965e14df7a94585c4fd6842b41f29a02a37b6
s-aarthi23 <s.aarthi2308@gmail.com>
2026-07-15 20:29:52 +0530

    Add files via upload

---
+++ python-day-9/day_9_mini_task.py
@@ -0,0 +1,24 @@
+s={1,2,3,6,7,8}
+r={5,6,7,8,9}
+print(s.intersection(r))
+
+s={1,2,3,6,7,8}
+r={5,6,7,8,9,9}
+h=set(s)
+y=set(r)
+print(h)
+print(y)
+
+s={1,2,3,6,7,8}
+r={5,6,7,8,9,9}
+print(s.union(r))
+
+s={1,2,3,6,7,8}
+r={5,6,7,8,9,9}
+check=3 in s
+print(check)
+
+s=[1,3,3,5,7,7,5,9,2,4]
+h=set(s)
+print(h)
+
---
+++ python-day-9/set_operations.py
@@ -0,0 +1,16 @@
+s={7,6,5}
+r={5,6,7}
+print(s.intersection(r))
+
+s={7,6,5}
+r={5,6,7}
+print(s.union(r))
+
+s={7,6,5}
+r={5,6,7}
+print(s.difference(r))
+
+s={7,6,5}
+r={5,6,8}
+print(s.symmetric_difference(r))
+
